51303217bb8cd4be679fe02bbbb13eb5cbb36b11
Roman Popov <pri20051982@gmail.com>
2021-12-27 00:28:24 +0300

    - Crashlytics setup - NaN is no more on stats screen

--- app/src/main/java/com/oftatech/superschoolboyremastered/viewmodel/StatisticsViewModel.kt
+++ app/src/main/java/com/oftatech/superschoolboyremastered/viewmodel/StatisticsViewModel.kt
@@ -37,12 +37,12 @@ class StatisticsViewModel @Inject constructor(private val spDao: StatisticsSPDao
 
     fun updateStatsData() {
         absoluteRightAnswersInRow.value = spDao.readAbsoluteRightAnswersInRow()
-        absoluteAverageResponseTime.value = spDao.readAbsoluteAverageResponseTime()
+        absoluteAverageResponseTime.value = if (spDao.readAbsoluteAverageResponseTime().isNaN()) 0F else spDao.readAbsoluteAverageResponseTime()
         lsTimer.value = spDao.readLsTimer()
         lsRightAnswers.value = spDao.readLsRightAnswers()
         lsWrongAnswers.value = spDao.readLsWrongAnswers()
         lsRightAnswersInRow.value = spDao.readLsRightAnswersInRow()
-        lsAverageResponseTime.value = spDao.readLsAverageResponseTime()
+        lsAverageResponseTime.value = if (spDao.readLsAverageResponseTime().isNaN()) 0F else spDao.readLsAverageResponseTime()
     }
 
     fun isEmpty(): Boolean {
